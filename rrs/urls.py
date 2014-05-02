05709c07436d038da27c11335bba0c4066b22ca2
Sean Francis N. Ballais <sfballais123@gmail.com>
2014-05-02 10:23:48 +0800

    Changed admin.py

--- rrs/urls.py
+++ rrs/urls.py
@@ -1,10 +1,5 @@
 from django.conf.urls import patterns, include, url
-<<<<<<< HEAD
 from login.views import user_login
-=======
-from login.views import UserPanel, hello
-
->>>>>>> 1a00bc241e1087cf3a15266eb5765240f10e1d0b
 from django.contrib import admin
 
 admin.autodiscover()
@@ -14,11 +9,6 @@ urlpatterns = patterns('',
     # url(r'^$', 'rrs.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-<<<<<<< HEAD
     url(r'^login/$', 'login.views.user_login'),
-=======
-    url(r'^login/', UserPanel),
-    url(r'^login/1', UserPanel),
->>>>>>> 1a00bc241e1087cf3a15266eb5765240f10e1d0b
     url(r'^admin/', include(admin.site.urls)),
 )
