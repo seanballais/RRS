dbfa42321cb7327591e0da12d706011024fe02a9
tramyar <raymart.montesclaros@yahoo.com>
2014-05-05 15:33:11 +0800

    Edited models.py in login with Reservation info

--- login/models.py
+++ login/models.py
@@ -4,14 +4,23 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class UserProfile(models.Model):
-	user 				= models.OneToOneField(User)
-	userID 				= models.AutoField(primary_key = True)
-	username			= models.CharField(max_length = 256, unique = True)
-	password			= models.CharField(max_length = 256)
-	emailadd			= models.CharField(max_length = 256)
-	user_privileges		= models.TextField()
-	firstname 			= models.TextField()
-	lastname			= models.TextField()
-	middlename			= models.TextField()
+	user 				              = models.OneToOneField(User)
+	userID 				              = models.AutoField(primary_key = True)
+	username			              = models.CharField(max_length = 256, unique = True)
+	password			              = models.CharField(max_length = 256)
+	emailadd			              = models.CharField(max_length = 256)
+	user_privileges		              = models.TextField()
+	firstname 			              = models.TextField()
+	lastname			              = models.TextField()
+	middlename			              = models.TextField()
 	def __unicode__(self):
 		return self.username
+class ReserveInfo(models.model):
+	EventID                           = models.AutoField(primary_key = True)
+	Event name                        = models.CharField(max_length = 256, unique = True)
+    Event description (optional)      = models.CharField(max_length = 256, blank = True)
+    Start Date                        = models.DateTimeField()
+    Start Time                        = models.DateTimeField()
+    End Date                          = models.DateTimeField()
+    End Time                          = models.DateTimeField()
+    Status                            = models.TextField()
