1043c2c471bef61627b556a1e5ac7cde4e4ad295
Sean Francis N. Ballais <sfballais123@gmail.com>
2014-05-08 11:57:23 +0800

    Extended user after X years

--- login/models.py
+++ login/models.py
@@ -6,13 +6,14 @@ from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, User
 # Create your models here.
 
 class CustomUserManager(BaseUserManager):
-    def create_user(self, username, password, emailadd):
+    def create_user(self, username, password, emailadd, user_privileges):
         if not username:
             raise ValueError('Users must have a username')
 
         user = self.model(
         	username = username,
             emailadd = CustomUserManager.normalize_email(emailadd),
+            user_privileges = user_privileges
         )
  
         user.set_password(password)
@@ -20,17 +21,14 @@ class CustomUserManager(BaseUserManager):
         return user
  
     def create_superuser(self, username, password, emailadd):
-        user = self.create_user(username, password = password, emailadd = emailadd)
-        user.is_superadmin = True
-        user_privileges = 1
+        user = self.create_user(username, password = password, emailadd = emailadd, user_privileges = 1)
 
         user.save(using=self._db)
         return user
 
 class CustomUser(AbstractBaseUser):
-	manager 						  = CustomUserManager()
-	user 							  = models.ForeignKey(settings.AUTH_USER_MODEL)
-	user_id 						  = models.AutoField(primary_key = True, unique = True)
+	objects 						  = CustomUserManager()
+	id 								  = models.AutoField(primary_key = True)
 	username			              = models.CharField('Username', max_length = 256, unique = True)
 	emailadd			              = models.EmailField('Email Address', max_length = 256)
 	UserPrivilegesChoices             = ((1, 'Superadmin'),(2, 'Admin'),(3, 'Faculty'),(4, 'Student'))
@@ -39,14 +37,10 @@ class CustomUser(AbstractBaseUser):
 	lastname			              = models.CharField('Last Name', max_length = 256)
 
 	USERNAME_FIELD = 'username'
-	REQUIRED_FIELDS = ['emailadd'] 
+	REQUIRED_FIELDS = ['emailadd']
 
 	def user_privileges_type(self):
 		return self.user_privileges
-
-	@property
-	def is_superadmin(self):
-		return self.is_admin # Check if user is a super admin or not
 
 	def __unicode__(self):
 		return self.username
