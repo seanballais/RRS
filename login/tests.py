280bcc0096c2dd8612030c15ecf76bf45f680796
Sean Francis N. Ballais <sfballais123@gmail.com>
2014-05-14 11:02:15 +0800

    Added a test

--- login/tests.py
+++ login/tests.py
@@ -5,11 +5,10 @@ from django.core.urlresolvers import reverse
 
 
 class LoginTest(TestCase):
-	""" Test that will create a test user and display the username """
     def create_user(self, username = "test", password = "12345", emailadd = "testing101@testing101.com", user_privileges = 1, firstname = "Test", lastname = "Culinary"):
         return CustomUser.objects.create(username=username,password=password,emailadd=emailadd, user_privileges=user_privileges, firstname=firstname, lastname=lastname)
 
     def test(self):
         w = self.create_user()
-        self.assertTrue(isinstance(w, Whatever))
+        self.assertTrue(isinstance(w, CustomUser))
         self.assertEqual(w.__unicode__(), w.username)
